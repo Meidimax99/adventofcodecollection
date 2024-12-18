036c7fc384cea5a6e97eb406e64958f25a0e63d9
Meidimax <max.meidinger99@gmail.com>
2024-12-18 09:44:10 +0100

    dump

--- 17/meidimax99/rust/src/main.rs
+++ 17/meidimax99/rust/src/main.rs
@@ -15,10 +15,12 @@ fn get_number(string: &str) -> usize {
     num.parse::<usize>().unwrap_or(0)
 }
 
-fn parse_input(contents: &String) -> Result<(Regs, Vec<Instr>), String> {
+fn parse_input(contents: &String) -> Result<(Regs, Vec<Instr>, Vec<usize>), String> {
     
     let mut regs = Regs {a:0 ,b:0, c:0};
     let mut instructions: Vec<Instr> = vec![];
+
+    let mut original_program: Vec<usize> = vec![];
 
     let mut lines = contents.lines();
 
@@ -39,20 +41,25 @@ fn parse_input(contents: &String) -> Result<(Regs, Vec<Instr>), String> {
 
     let mut nums_iter = nums.into_iter();
     let mut elem = nums_iter.next().unwrap();
+    let mut index = 0;
     loop {
-        let mut instr = Instr{opcode: OpCode::Adv, operant: 0};
+        original_program.push(elem as usize);
+        let entry: Vec<i32> = vec![index-1];
+        let mut instr = Instr{opcode: OpCode::Adv, operant: 0, index: index as u8, entry};
         instr.opcode = OpCode::from_u8(elem).unwrap();
         instr.operant = nums_iter.next().unwrap();
+        original_program.push(instr.operant as usize);
         let next = nums_iter.next();
         instructions.push(instr);
         if next.is_some() {
             elem = next.unwrap();
+            index += 1;
         } else {
             break;
         }
 
     }
-    return Ok((regs, instructions));
+    return Ok((regs, instructions, original_program));
 }
 
 fn read_file(path: &String) -> Result<String, String> {
@@ -76,7 +83,7 @@ fn read_file(path: &String) -> Result<String, String> {
     return Ok(contents)
 }
 
-#[derive(Debug, FromPrimitive)]
+#[derive(Debug, FromPrimitive, PartialEq)]
 enum OpCode {
     Adv = 0,
     Bxl,
@@ -100,6 +107,8 @@ struct Regs {
 struct Instr {
     opcode: OpCode,
     operant: u8,
+    index: u8,
+    entry: Vec<i32>,
 }
 
 
@@ -108,6 +117,7 @@ impl Instr {
     fn execute(&self, regs: &mut Regs, pc: &mut usize) -> Option<usize>{
         match self.opcode {
             OpCode::Adv => {
+                // Divide value in register by 2 to the power of the combo value, store to a
                 let numerator = regs.a;
                 let denominator = (2 as usize).pow(self.get_op_combo(regs) as u32);
                 regs.a = (numerator as f64 / denominator as f64) as usize;
@@ -115,17 +125,20 @@ impl Instr {
                 None
             },
             OpCode::Bxl => {
+                // Xor B with operant and store it to b
                 regs.b = regs.b ^ self.operant as usize;
                 *pc += 1;
                 None
             },
             OpCode::Bst => {
+                // Store Combo value % 8 to b
                 let comb = self.get_op_combo(regs) % 8;
                 regs.b = comb;
                 *pc += 1;
                 None
             },
             OpCode::Jnz => {
+                //Program counter to operant
                 if regs.a == 0 {
                     *pc += 1;
                 } else {
@@ -134,16 +147,19 @@ impl Instr {
                 None
             },
             OpCode::Bxc => {
+                //Xor b and c and store to b
                 regs.b = regs.b ^ regs.c;
                 *pc += 1;
                 None
             },
             OpCode::Out => {
+                //Print
                 let out = self.get_op_combo(regs) % 8;
                 *pc += 1;
                 Some(out)
             },
             OpCode::Bdv => {
+                //Same as adv but store to b
                 let numerator = regs.a;
                 let denominator = (2 as usize).pow(self.get_op_combo(regs) as u32);
                 regs.b = (numerator as f64 / denominator as f64) as usize;
@@ -151,6 +167,7 @@ impl Instr {
                 None
             },
             OpCode::Cdv => {
+                //Same as adv but store to c
                 let numerator = regs.a;
                 let denominator = (2 as usize).pow(self.get_op_combo(regs) as u32);
                 regs.c = (numerator as f64 / denominator as f64) as usize;
@@ -192,14 +209,45 @@ fn run(regs: &mut Regs, instructions: Vec<Instr>) {
         }
     }
 }
+
+fn find_instr(instructions: &Vec<Instr>, instr_type: OpCode) -> Vec<u8> {
+
+    let mut instr_idx = vec![];
+
+    for (idx, ele) in instructions.iter().enumerate() {
+        if ele.opcode == instr_type {
+            instr_idx.push(idx as u8);
+        }
+    }
+    return instr_idx;
+}
+
+fn reverse_engineer(instructions: &mut Vec<Instr>, desired_output: &Vec<usize>) -> Option<usize> {
+    // //Assumption: Only one Print Statement
+
+    // //Find Print statements
+    // let print_statements_idx = find_instr(&instructions, OpCode::Out);
+    // assert!(print_statements_idx.len() == 1);
+    // //Find jump statements
+    // let jump_st_idx = find_instr(&instructions, OpCode::Jnz);
+
+    // //Add entry information to jumps
+    // for idx in jump_st_idx {
+    //     let target = instructions[idx as usize].op;
+    // }
+    None
+}
 fn main() {
 
     let path = String::from("input.txt");
 
-    let (mut regs, instructions) = parse_input(&read_file(&path).unwrap()).unwrap();
+    let (mut regs, mut instructions, prog) = parse_input(&read_file(&path).unwrap()).unwrap();
 
-    run(&mut regs, instructions);
+    //run(&mut regs, instructions);
 
-    println!()
+    println!();
+    let a  = reverse_engineer(&mut instructions, &prog).unwrap_or(0);
+
+    println!("A = {} lets the program replicate itself", a);
 
 }
---
+++ 17/meidimax99/rust/src/reverse_engineer.md
@@ -0,0 +1,8 @@
+2,4,    Bst     Store Value of Register A % 8 in B
+1,3,    Bxl     Xor Value register B with 3
+7,5,    Cdv     c = a / 2^b
+0,3,    Adv     a = a / 2^3
+4,3,    Bxc     b = b ^ c
+1,5,    Bxl     b = b ^ 5
+5,5,    Out     print b % 8
+3,0     Jnz     jump to 0
